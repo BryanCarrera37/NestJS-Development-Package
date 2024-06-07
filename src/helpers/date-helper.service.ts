371e28c651d64fb80c0537799e1d3f3c82c7024e
Bryan Carrera <bc00932@gmail.com>
2024-06-06 23:08:27 -0400

    fix(DEV): Importing the right package for Dates and adding a new method to get a Date reduced

--- src/helpers/date-helper.service.ts
+++ src/helpers/date-helper.service.ts
@@ -1,13 +1,22 @@
 import { Injectable } from "@nestjs/common";
-import * as moment from 'momment';
+import * as moment from 'moment';
 
 @Injectable()
 export class DateHelperService {
 
+    private static readonly defaultFormat = 'YYYY-MM-DD HH:mm:ss';
+
     getCurrentDate(format?: string): string {
         return moment().format(format != undefined
             ? format
-            : 'YYYY-MM-DD');
+            : DateHelperService.defaultFormat);
+    }
+
+    getDateReduced(daysToReduce: number, format?: string): string {
+        return moment().subtract(daysToReduce, 'days').format(format != undefined
+            ? format
+            : DateHelperService.defaultFormat
+        );
     }
     
     getTheDiffInDays(dateToCompare: string): number {
