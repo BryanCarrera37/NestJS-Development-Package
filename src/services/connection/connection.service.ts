ad415b8e338f20c7dc43e5bbdf9dcd16ca706a4b
Bryan Carrera <bc00932@gmail.com>
2024-06-14 00:35:48 -0400

    feat(DEV): Enabling the Connection Services to have a promise that will specify if the connection were established

--- src/services/connection/connection.service.ts
+++ src/services/connection/connection.service.ts
@@ -6,10 +6,17 @@ export abstract class ConnectionService {
 
     private static _connection: DataSource;
 
+    private resolveConnectionEstablished: () => void;
+
     protected readonly environmentHelper: EnvironmentHelperService;
     protected readonly databaseResultHelper: DatabaseResultHelperService;
     protected logger: Logger;
     protected connection: DataSource;
+
+    /**
+     * It consists of been a promise that you can use to determine when the connection is established
+     */
+    public connectionEstablished: Promise<void>;
 
     constructor() {
         this.environmentHelper = new EnvironmentHelperService();
@@ -17,10 +24,21 @@ export abstract class ConnectionService {
         this.initializeConnection();
     }
 
+    /**
+     * You have to call this method in your constructor with the objective of initialize the class's logger
+     */
     protected abstract initializeLogger(): void;
+
+    /**
+     * You only have to define this method. The method is called by the **initializeConnection** method to establish the connection with the database
+     */
     protected abstract getDataSource(): DataSource;
 
+    /**
+     * This method is called in the constructor and it's responsible to initialize the database connection given in the abstract method named **getDataSource**
+     */
     protected async initializeConnection() {
+        this.initializeResolveConnectionEstablished();
         if(!ConnectionService._connection) {
             ConnectionService._connection = this.getDataSource();
         }
@@ -30,5 +48,12 @@ export abstract class ConnectionService {
         }
 
         this.connection = ConnectionService._connection;
+        this.resolveConnectionEstablished();
+    }
+
+    private initializeResolveConnectionEstablished(): void {
+        this.connectionEstablished = new Promise<void>((resolve) => {
+            this.resolveConnectionEstablished = resolve;
+        });
     }
 }
